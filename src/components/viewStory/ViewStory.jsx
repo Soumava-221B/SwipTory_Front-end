b91dd01af8be2bd09da5f0159156861257ff7c4b
soumava <soumavadas06@gmail.com>
2024-04-28 19:41:04 +0530

    share link fixed

--- src/components/viewStory/ViewStory.jsx
+++ src/components/viewStory/ViewStory.jsx
@@ -85,7 +85,7 @@ export const ViewStory = ({
 
   const handleCopyStoryLink = () => {
     navigator.clipboard.writeText(
-      `http://localhost:3000/viewStory/${story?._id}`
+      `https://swip-tory-front-end.netlify.app/viewstory/${story?._id}`
     );
     toast.success("Link copied successfully!");
   };

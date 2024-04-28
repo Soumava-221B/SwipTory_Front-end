72bdc5b8cd064bc29a14f7d1cf3c340577f3dc4b
soumava <soumavadas06@gmail.com>
2024-04-28 19:02:21 +0530

    share stories fix

--- src/components/topStories/TopStories.jsx
+++ src/components/topStories/TopStories.jsx
@@ -4,15 +4,32 @@ import { categoryData } from "../../utils/cardsData";
 import { useActiveFilter } from "../../providers/activeFilterProvider";
 import YourStories from "../../pages/yourStories/YourStories";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import newRequest from "../../utils/newRequest";
 
 export const TopStories = () => {
   const { activeFilter } = useActiveFilter();
 
   const { currentUser } = useSelector((state) => state.user);
 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchD = async () => {
+      try {
+        const d = await newRequest.get(`/user/getMyStories`);
+        setData(d?.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchD();
+  }, []);
+
   return (
     <div className={styles.container}>
-      {currentUser && <YourStories />}
+      {currentUser && data.length > 0 && <YourStories />}
 
       {categoryData.map(
         (card) =>
